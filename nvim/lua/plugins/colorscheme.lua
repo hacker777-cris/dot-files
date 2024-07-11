d42a847987df93896b20aa10d5ff28a9a29813d0
hacker777-cris <crispusgikonyo@gmail.com>
2024-07-11 20:46:43 +0300

    Colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,15 @@
+-- lua/plugins/colorscheme.lua
 return {
 	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
+		"catppuccin/nvim",
+		as = "catppuccin",
+		lazy = false,
 		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+			})
+			vim.cmd("colorscheme catppuccin")
 		end,
 	},
 }
